84dbcaff8f76a852705d1cc6cd3deaddf89c47a3
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 12:17:19 -0300

    correcao dos exercicios

--- src/Exercicio5.java
+++ src/Exercicio5.java
@@ -1,8 +1,8 @@
 public class Exercicio5 {
     public static void main(String[] args) {
         double valorEmDolares = 70.5;
-        double valorDoDolarEmReais = 4.94;
-        double conversao = valorEmDolares * valorDoDolarEmReais;
+        double taxaDeConversao = 4.94;
+        double conversao = valorEmDolares * taxaDeConversao;
 
         System.out.println("O valor $" + valorEmDolares + " dólares convertido em reais é R$" + conversao + ".");
     }
