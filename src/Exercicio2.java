9e93f2ec20f068b41e3da0b5cdb9164712f3d01a
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 11:39:08 -0300

    exercicio 2

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,5 +1,8 @@
 public class Exercicio2 {
     public static void main(String[] args) {
-        double =
+        double variavel1 = 5.5;
+        int variavelConvertida = (int) variavel1;
+
+        System.out.println("A variável double " + variavel1 + " convertida em inteiro é " + variavelConvertida);
     }
 }
