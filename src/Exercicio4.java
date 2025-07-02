7ac87acee99221ff4abc39fd327558c0c1c2114f
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 11:59:25 -0300

    Exercicio 4

---
+++ src/Exercicio4.java
@@ -0,0 +1,10 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        double precoProduto = 35.6;
+        int quantidade = 4;
+        double quantidadeConvertida = (int) quantidade;
+        double total = precoProduto * quantidadeConvertida;
+
+        System.out.println("Total: " + total);
+    }
+}
