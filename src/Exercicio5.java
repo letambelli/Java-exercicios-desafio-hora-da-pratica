c922566040c9694f822bac54e9c36cc4364231c0
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 12:07:37 -0300

    Exercicio 5

---
+++ src/Exercicio5.java
@@ -0,0 +1,9 @@
+public class Exercicio5 {
+    public static void main(String[] args) {
+        double valorEmDolares = 70.5;
+        double valorDoDolarEmReais = 4.94;
+        double conversao = valorEmDolares * valorDoDolarEmReais;
+
+        System.out.println("O valor $" + valorEmDolares + " dólares convertido em reais é R$" + conversao + ".");
+    }
+}
