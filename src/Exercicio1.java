7dc120c5b7a698e55c1febc1dc0220a20b6dbd23
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 11:35:41 -0300

    primeiro commit

---
+++ src/Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double nota1 = 5.5;
+        double nota2 = 8.9;
+        double media = (nota1 + nota2)/2;
+
+        System.out.println("A média das notas " + nota1 + " e " + nota2 + " é igual a " + media + ".");
+    }
+}
