fc779ec06b2f8e621c66439918360cec201de695
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-02 11:47:27 -0300

    Exercicio 3

---
+++ src/Exercicio3.java
@@ -0,0 +1,9 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        char letra = 'A';
+        String frase = " frase completa a seguir.";
+
+        System.out.println(letra + frase);
+
+    }
+}
